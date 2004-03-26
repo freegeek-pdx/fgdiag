ee0e08747551e22917da0d610b197800e9173112
chromakode <chromakode@dev.freegeek.org>
2004-03-26 04:58:39 +0000

    *** empty log message ***

---
+++ bin/testscript.py
@@ -0,0 +1,13 @@
+"""Testing API Tester
+
+Runs through some functions of the testing api.
+
+"""
+
+from fgdiag.lib import testing
+
+def __run__(gizmo):
+	print gizmo.manufacturer
+	return {'working':True}
+	
+if __name__ == '__main__': testing.start_test(__run__)
