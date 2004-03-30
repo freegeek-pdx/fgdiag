f50fd224481ca230c454813f44ed5208d8788fca
chromakode <chromakode@dev.freegeek.org>
2004-03-30 02:34:15 +0000

    MySQLdb beginnings

---
+++ fgdiag/lib/testing/FGDB.skel.py
@@ -0,0 +1,50 @@
+"""FreeGeek Database access."""
+
+import MySQLdb
+
+def connect(dburl):
+    """Return a Database instance connected to dburl.
+    
+    Attempt to connect to dburl, and fail gracefully by falling back to a temporary file cache.
+    
+    """
+    try:
+            # (Insert connection code here)
+            # db = something
+            pass
+    except:
+            # (Insert graceful failure here) ;)
+            pass
+    
+    return db
+     
+class Database:
+    def __init__(self, conn):
+            self.__conn = conn
+            self.__field_map = FieldMap(self.__conn)
+
+    def get_gizmo_by_id(gid):
+            """Get a Gizmo and return an object."""
+    
+    def __get_conn():
+            return self.__conn
+            
+    def __get_field_map():
+            return self.__field_map
+    
+    connection = property(__get_conn, doc='Connection object for the Database.')
+    field_map = property(__get_field_map, doc='FieldMap of the Database.')
+
+class Gizmo:
+    # FIXME: 
+    # For SQLObject implementation, take SQLObject instance in __init__
+    # For MySQLdb, take MySQLdb connection.
+            
+    def register_test_data(data):
+            """Put data into appropriate fields in database."""
+
+class FieldMap:
+    # Same as Gizmo
+       
+    def get_field_location(fieldname):
+        """Return the Table fieldname belongs to."""
--- fgdiag/lib/testing/__init__.py
+++ fgdiag/lib/testing/__init__.py
@@ -1,10 +1,14 @@
 """Python Module for FreeGeek testing."""
 
-FGDBURL = 'mysql://chromakode@localhost/chromakode'
+FGDBHOST = 'localhost'
+FGDBDB = 'fgdbtest'
+FGDBUSER = 'fgdbtester'
+# Is it wise to have password coded in? Plaintext?
+FGDBPASSWD = 'freegeek'
 
 def start_test(runfunc):
     """Test a Gizmo.
-
+    
     Steps Taken:
     1.  Connect to FGDB.
     2.  Prompt for a Gizmo ID.
@@ -12,21 +16,21 @@ def start_test(runfunc):
     4.  Run a test in the form of a given function runfunc.
     5.  Process returned test data and translate parameters into Table locations.
     6.  Put the data into FGDB under the selected Gizmo.
-
+    
     Keyword arguments:
     runfunc -- function that implements a test
-
+    
     """
-
+    
     from FGDB import connect
     from Prompts import prompt_for_gizmo
     from TestData import process
-
-    db = connect(FGDBURL)
+    
+    db = connect(FGDBHOST, FGDBDB, FGDBUSER, FGDBPASSWD)
     gid = prompt_for_gizmo()
     gizmo = db.get_gizmo_by_id(gid)
-
+    
     data = runfunc(gizmo)
     data = process(data, db.field_map)
-
+    
     gizmo.register_test_data(data)
