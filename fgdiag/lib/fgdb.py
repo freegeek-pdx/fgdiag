1370757032d0d1a470d84dbbea4ea29b2c21d6fc
chromakode <chromakode@dev.freegeek.org>
2004-04-02 23:55:26 +0000

    FieldMap Tables, Cross-Table Gizmos, and lots of other crazy stuff.

--- fgdiag/lib/fgdb.py
+++ fgdiag/lib/fgdb.py
@@ -2,22 +2,19 @@
 
 Example:
     
->>> db = fgdb.connect('localhost','fgdbtest','fgdbtester','freegeek')
->>> cd = db.get_table("CDDrive")
->>> row = cd.get_row(72558)
->>> row.get_column(("scsi",))
-('N',)
+>>> db = fgdb.connect("host","database","user","passwd")
 >>> gizmo = db.get_gizmo_by_id(72558)
->>> db.field_map.get_field_location(gizmo.class_tree.split('.'), "working")
-'Gizmo'
->>> db.field_map.get_field_location(gizmo.class_tree.split('.'), "notes")
-'Gizmo'
->>> gizmo.set_column({"notes":"FreeGeek Database Python Access Is Working"})
+>>> gizmo.class_tree
+'Gizmo.Component.Drive.CDDrive'
+>>> gizmo.get("id","notes","value","speed","interface","inSysId")
+(72558L, '', 0.0, '', '', 0L)
+>>> gizmo.set(notes="FreeGeek Database Python Access Is Working")
 True
->>> gizmo.get_column(("notes",))
-('FreeGeek Database Python Access Is Working',)
+>>> g.get("notes")
+'FreeGeek Database Python Access Is Working'
 >>> gizmo.table
-<fgdb.Table instance at 0x4020306c>
+<fgdb.FieldMapMultipleTable instance at 0x402b2c2c>
+
 """
 
 import MySQLdb
@@ -38,32 +35,64 @@ def connect(host, db, user, passwd):
     
     return mydb
 
-class Factory:
-    """
-    Generic class factory
+def _equals(field, value):
+    return "%s = '%s'" % (field, value)
+
+def _list_to_string(l):
+    return ", ".join(l)
+
+def _list_to_AND_string(l):
+    return " AND ".join(l)
+
+def _value_dict_to_string(valuedict):
+    sets = tuple()
+    for item in valuedict.iteritems():
+        sets += (_equals(item[0],item[1]),)
+    return _list_to_string(sets)
     
-    Creates of instances of a class and keeps them for further use in a 
-    list. If an instance with the same initial arguments already exists,
-    it will return the existing instance. Supports in-place substitution
-    with an existing class.
+def _id_values(tables, id_, idname="id"):
+    sets = tuple()
+    for table in tables:
+        sets += (_equals(table + "." + idname, id_),)
+    return _list_to_AND_string(sets)
+    
+def _single_select_sql(fields, table, where):
+    return "SELECT %s FROM %s WHERE %s" % (_list_to_string(fields), table, where)
+
+def _single_update_sql(valuedict, table, where):
+    return "UPDATE %s SET %s WHERE %s" % (table, _value_dict_to_string(valuedict), where)
+
+def _multiple_select_sql(fields, tables, where):
+    return "SELECT %s FROM %s WHERE %s" % (_list_to_string(fields), _list_to_string(tables), where)
+
+def _multiple_update_sql(valuedict, tables, where):
+    return "UPDATE %s SET %s WHERE %s" % (_list_to_string(tables), _value_dict_to_string(valuedict), where)
+
+class Cache:
+    """
+    Generic class/function cache
+    
+    Calls a callable and keeps the result for further use in a dictionary. 
+    If an instance with the same initial arguments already exists, it will 
+    return the existing result. Supports in-place substitution with an 
+    existing class.
     """
     # Should this go in pyutil?
-    # I was going to use this for Tables but decided against it, may be useful later.
     # Question: Which if any of the below classes would benefit from this?
     # None currently have any stored state, but that may change in the future.
         
-    def __init__(self, cls):
-        self.__cls = cls
-        self.__existing = dict()
+    def __init__(self, call):
+        self.__call = call
+        self.__results = dict()
         
     def __call__(self, *args, **kwds):
         return self.get(*args, **kwds)
     
     def get(self, *args, **kwds):
         totalargs = args + tuple(kwds.keys()) + tuple(kwds.values())
-        if not self.__existing.has_key(totalargs):
-            self.__existing[totalargs] = self.__cls(*args, **kwds)
-        return self.__existing[totalargs] 
+        if not self.__results.has_key(totalargs):
+            self.__results[totalargs] = self.__call(*args, **kwds)
+        return self.__results[totalargs] 
     
 class Database:
     def __init__(self, conn):
@@ -73,20 +102,42 @@ class Database:
     def get_table(self, name, idname = "id"):
         """Get Table object by name."""
         return Table(self, name, idname)
+    
+    def get_multiple_tables(self, names, idname = "id"):
+        """Get MultipleTable object by a tuple of names."""
+        return MultipleTable(self, names, idname)
+        
+    def get_field_map_tables(self, names, idname = "id"):
+        """Get FieldMapTable object by a tuple of names."""
+        return FieldMapMultipleTable(self, names, idname)
         
     def get_gizmo_by_id(self, gid):
         """Get a Gizmo and return an object."""
         return Gizmo(self, gid)
     
     def query_one(self, sql):
-        c = self.__conn.cursor()
-        c.execute(sql)
-        return c.fetchone()
+        c = self.__try_execute(sql)
+        return self.__process_result(c.fetchone())
     
     def execute(self, sql):
-        c = self.__conn.cursor()
-        c.execute(sql)
+        self.__try_execute(sql)
         return True
+    
+    def __try_execute(self, sql):
+        c = self.__conn.cursor()
+        try:
+            c.execute(sql)
+        except MySQLdb.ProgrammingError:
+            print "SQL call failed: " + sql
+            # FIXME: Fall back here?
+            raise
+        return c
+    
+    def __process_result(self, result):
+        if len(result)==1:
+            return result[0]
+        else:
+            return result
         
     def __get_conn(self):
         return self.__conn
@@ -98,49 +149,91 @@ class Database:
     field_map = property(__get_field_map, doc="FieldMap of the Database.")
 
 class Table:
-    def __init__(self, db, name, idname = "id"):
+    """Generates queries for a table."""
+    def __init__(self, db, table, idname = "id"):
         self.__db = db
-        self.__name = name
+        self.__table = table
         self.__idname = idname
     
     def get_row(self, id_):
         return TableRow(self, id_, self.__idname)
         
-    def get_column_by_id(self, id_, fields):
-        sql = "SELECT %s FROM %s WHERE %s = %s" % (", ".join(fields), self.__name, self.__idname, id_)
+    def get_by_id(self, id_, fields):
+        sql = _single_select_sql(fields, self.table, _equals(self.__idname, id_))
         return self.__db.query_one(sql)
     
-    def set_column_by_id(self, id_, fields):
-        sets = tuple()
-        for item in fields.iteritems():
-            sets = sets + ("%s = '%s'" % (item[0], item[1]),)
-        sql = "UPDATE %s SET %s WHERE %s = %s" % (self.__name, ', '.join(sets), self.__idname, id_)
+    def set_by_id(self, id_, valuedict):
+        sql = _single_update_sql(valuedict, self.__table, _equals(self.__idname, id_))
         return self.__db.execute(sql)
         
-    def __get_name(self):
-        return self.__name
+    def __get_table(self):
+        return self.__table
     
     def __get_database(self):
         return self.__db    
     
-    name = property(__get_name, doc="Name of Table in the Database.")
+    table = property(__get_table, doc="Name of Table in the Database.")
     database = property(__get_database, doc="Database containing the Table.")
+
+class MultipleTable:
+    """Generates queries spanning multiple tables."""
+    def __init__(self, db, tables, idname = "id"):
+        self.__db = db
+        self.__tables = tables
+        self.__idname = idname
+    
+    def get_row(self, id_):
+        # TableRow can stay unchanged because it just wraps the Table object given to it.
+        return TableRow(self, id_, self.__idname)
+        
+    def get_by_id(self, id_, fields):
+        sql = _multiple_select_sql(fields, self.__tables, _id_values(self.tables, id_, self.__idname))
+        return self.__db.query_one(sql)
+    
+    def set_by_id(self, id_, valuedict):
+        sql = _multiple_update_sql(valuedict, self.__tables, _id_values(self.tables, id_, self.__idname))
+        return self.__db.execute(sql)
+        
+    def __get_tables(self):
+        return self.__tables
+    
+    def __get_database(self):
+        return self.__db    
+    
+    tables = property(__get_tables, doc="Names of Tables in the Database.")
+    database = property(__get_database, doc="Database containing the Tables.")
+    
+class FieldMapMultipleTable(MultipleTable):
+    """Represents multiple Tables and looks up field name mapping using the FGDB fieldMap."""
+    
+    def __init__(self, db, tables, idname = "id"):
+        MultipleTable.__init__(self, db, tables, idname)
+        
+    def get_by_id(self, id_, fields):
+        return MultipleTable.get_by_id(self, id_, self.database.field_map.process_field_list(self.tables, fields))
+        
+    def set_by_id(self, id_, valuedict):
+        return MultipleTable.set_by_id(self, id_, self.database.field_map.process_value_dict(self.tables, valuedict))
 
 class TableRow:
     def __init__(self, tb, id_, idname = "id"):
         self.__tb = tb
         self.__id = id_
     
-    def get_column(self, fields):
-        return self.__tb.get_column_by_id(self.__id, fields)
+    def get(self, *fields):
+        return self.__tb.get_by_id(self.__id, fields)
     
-    def set_column(self, fields):
-        return self.__tb.set_column_by_id(self.__id, fields)
+    def set(self, valuedict=None, **keywordvaluedict):
+        if valuedict!=None:
+            vd = valuedict
+        else:
+            vd = keywordvaluedict
+        return self.__tb.set_by_id(self.__id, vd)
     
     def __get_table(self):
         return self.__tb
     
-    def __get_table(self):
+    def __get_id(self):
         return self.__id
     
     id = property(__get_id, doc="ID of the TableRow.")
@@ -148,11 +241,14 @@ class TableRow:
     
 class Gizmo(TableRow):	
     def __init__(self, db, gid):
-        # This is where I was planning to use Factory; is it bad to be passing each Gizmo a different Table? I think it is (bad)...
-        TableRow.__init__(self, db.get_table("Gizmo"), gid)   
+        # This is where I was planning to use Cache; is it bad to be passing each Gizmo a different Table instance? I think it is (bad)...
+        # Chicken and the Egg: Needs class_tree to initialize TableRow, TableRow needs to be initialized to get class_tree. I'll make it create a temporary tablerow and then use that to initialize TableRow.
+        temptablerow = TableRow(db.get_table("Gizmo"), gid)
+        class_tree = tuple(temptablerow.get("classTree").split("."))
+        TableRow.__init__(self, db.get_field_map_tables(class_tree), gid)   
         
     def __get_class_tree(self):
-        return self.get_column(("classTree",))[0]
+        return self.get("classTree")
     
     class_tree = property(__get_class_tree, doc="Class Tree of the Gizmo.")
 
@@ -160,10 +256,31 @@ class FieldMap(Table):
     def __init__(self, db):
         Table.__init__(self, db, "FieldMap")
         
-    def get_field_location(self, classes, fieldname):
+    def __get_field_location(self, classes, fieldname):
         """Return the Table fieldname belongs to."""
         sql = "SELECT tableName FROM fieldMap WHERE tableName IN %s AND fieldName='%s'" % (repr(tuple(classes)), fieldname)
-        # Table because according to the private var naming __db belongs to table...
-        # Fix by making __db not private or keeping separate __dbs?
-        tablename = self.database.query_one(sql)[0]
+        tablename = self.database.query_one(sql)
         return tablename
+    
+    # Cache stuff
+    __get_field_location_cache = Cache(__get_field_location)
+    
+    def get_field_location(self, classes, fieldname):
+        return self.__get_field_location_cache(self, classes, fieldname)
+    #---
+    
+    def process_field_list(self, classes, fieldlist):
+        """Convert a list like ("id","speed") to ("Gizmo.id","CDDrive.speed")"""
+        newfieldlist = tuple()
+        for f in fieldlist:
+            newfieldlist += (self.get_field_location(classes, f) + "." + f,)
+        return newfieldlist
+        
+    def process_value_dict(self, classes, valuedict):
+        """Convert a dictionary like {"id":12345,"speed":4) to ("Gizmo.id":12345,"CDDrive.speed":4)"""
+        newvaluedict = dict()
+        for item in valuedict.iteritems():
+            newvaluedict[self.get_field_location(classes, item[0]) + "." + item[0]] = item[1]
+        return newvaluedict
+        
+__all__ = ["connect", "Database", "Table", "TableRow", "MultipleTableRow", "Gizmo", "FieldMap"]
