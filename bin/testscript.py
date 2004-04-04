a844b6db48860c589e7e2f7c5def7e3d09bf2dce
kevint <kevint@dev.freegeek.org>
2004-04-04 01:45:17 +0000

    s/fgdiag.lib.testing/fgdiag.lib.test/

--- bin/testscript.py
+++ bin/testscript.py
@@ -4,10 +4,11 @@ Runs through some functions of the testing api.
 
 """
 
-from fgdiag.lib import testing
+from fgdiag.lib import test
 
 def __run__(gizmo):
     print gizmo.manufacturer
     return {'working':True}
 
-if __name__ == '__main__': testing.start_test(__run__)
+if __name__ == '__main__':
+    test.start_test(__run__)
