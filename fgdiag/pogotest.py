6a36ecc63372e0de88fed6c231c33a8723ebbf57
kevint <kevint@dev.freegeek.org>
2004-05-05 03:14:42 +0000

    whitespace noramlization

--- fgdiag/pogotest.py
+++ fgdiag/pogotest.py
@@ -8,7 +8,7 @@ from lib import test, userinteraction
 
 def pogo_scan():
     # Define code to find avaliable Pogo Sticks here. Since there is no feasible
-    # way to detect this from the computer, just create a PogoDevice instance 
+    # way to detect this from the computer, just create a PogoDevice instance
     # and stick it in a sequence.
     return (PogoDevice(),)
 
@@ -21,28 +21,28 @@ class PogoDevice(test.TestableDevice):
         # Define code to Test a Pogo Stick here
         working = userinteraction.yesno("Does the pogo stick work?", "Please bring the pogo stick to a hard and flat surface and try it out. Does it work?")
         return test.status_from_boolean(working)
-        
+
     def _d_data(self):
         # Define code to get data on a Pogo Stick here
         color = userinteraction.prompt("Color?", "What color is the pogo stick?")
         return {"notes":"Colored %s"%color}
-        
+
     def _d_description(self, data):
-        # Define code to make a descriptiong for a Pogo Stick here
+        # Define code to make a description for a Pogo Stick here
         return data["notes"]
-        
+
 class PogoTester(test.GizmoTester):
 
     #gizmotype = "Gizmo.Other"
     gizmotype = "Gizmo.Component.Printer"
-    
+
     def run(self):
-        # Define test logic for a Pogo Stick here. Usually this is pretty 
+        # Define test logic for a Pogo Stick here. Usually this is pretty
         # straightforward, like in this case.
         pogos = pogo_scan()
         for pogo in pogos:
-            pogo.get_data()   
-            status = pogo.test()        
+            pogo.get_data()
+            status = pogo.test()
         return pogos
 
 def main():
