9f8a706b056fdd8ce23346794ce7070deda26c9f
chromakode <chromakode@dev.freegeek.org>
2004-05-13 00:15:59 +0000

    Cleanup

--- fgdiag/lib/logging.py
+++ fgdiag/lib/logging.py
@@ -1,8 +1,11 @@
+import atexit
+
 log=list()
-_nodes = dict()
+
+_nodenames = dict()
 
 printoutput = False
-#printoutput = True
+printoutput = True
 
 def _format(nodes, title, message):
    nodestr = "[%s]" % "|".join([node for node in nodes])
@@ -14,12 +17,12 @@ class _LogNode:
    Do not instance this class directly; use create_node and child_node
    """
    def __init__(self, basename, description=None, parents=tuple()):
-      if not _nodes.has_key(basename):
-         _nodes[basename] = 0
+      if not _nodenames.has_key(basename):
+         _nodenames[basename] = 0
          self.name = basename
       else:
-         _nodes[basename] += 1
-         self.name = basename + str(_nodes[basename])
+         _nodenames[basename] += 1
+         self.name = basename + str(_nodenames[basename])
       if description:
          self.name += "(%s)" % description
       self.nodetree = (parents + (self.name,))
@@ -37,9 +40,6 @@ class _LogNode:
    def child_node(self, name, description=None):
       return _LogNode(name, description, self.nodetree)
 
-_basenode = _LogNode("base")
-_basenode("Start", "Logging Started.")
-
 def create_node(name, description=None):
    """Create a new log node."""
    return _basenode.child_node(name, description)
@@ -50,3 +50,16 @@ def dump():
    for nodes, title, message in log:
       text += _format(nodes, title, message)+"\n"
    return text
+
+_basenode = _LogNode("base")
+_basenode("Start", "Logging Started.")
+
+def exit():
+   global _basenode
+   
+   _basenode("Finish", "Logging Finished")
+
+   del _basenode
+   # Save log?
+
+atexit.register(exit)
