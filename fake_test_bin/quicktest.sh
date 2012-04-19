59ff362405b4d2434922b5ce85c338e27079dbe3
Ryan Niebur <ryanryan52@gmail.com>
2012-04-19 13:54:46 -0700

    Implement DISKTEST_TIME_LIMIT_PER_GB=6 to limit drive tests to 6 minutes per gigabyte of storage and abort the test (Closes: #27911)

--- fake_test_bin/quicktest.sh
+++ fake_test_bin/quicktest.sh
@@ -2,6 +2,11 @@
 
 # used on a xen virtual machine to very easily test disktest (smartctl and lshw have problems normally)
 
+EXTRA_OPTS=""
+EXTRA_OPTS="DISKTEST_TIME_LIMIT_PER_GB=10"
+
 set -e
 
-sudo env PATH=./fake_lshw_bin_for_tests:/sbin:$PATH RUBYLIB=lib DISKTEST_REQUIRED_NUMBER_OF_DRIVES=1 DISKTEST_DISK_REGEX=xvdb ./bin/disktest
+TEST_BASE=$(dirname $(readlink -f $0))
+TEST_LIB=$(readlink -f ${TEST_BASE}/../lib)
+sudo env PATH=$TEST_BASE:/sbin:$PATH RUBYLIB=$TEST_LIB DISKTEST_REQUIRED_NUMBER_OF_DRIVES=1 DISKTEST_DISK_REGEX=xvdb $EXTRA_OPTS ./bin/disktest
